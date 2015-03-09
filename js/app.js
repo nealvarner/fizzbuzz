a18f83ba4645023f56be1b95d67dda748794bd2f
Neal Varner <neal.varner@gmail.com>
2015-03-09 01:13:07 -0500

    refactored fizzbuzz into FizzBuzz object with methods.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,43 @@
-jQuery(document).ready(function($) {
+function fizzbuzz(max) {
 
-  for (var i = 0; i <=100; i++){
-    var output = ''
-    if (i % 3 == 0) {
-      output += "fizz"
-    }
-    if (i % 5 == 0) {
-      output += "buzz"
-    }
-    if (output == '') {
-      output = String(i)
-    }
-
-    $('ul').append('<li>'+output+'</li>')
-
+  function check(n) {
+    var output = '';
+    if ( n % 3 == 0 ) { output += "Fizz"}
+    if ( n % 5 == 0 ) { output += "Buzz"}
+    return output || n;
   }
+
+  function checkVaild(max) {
+    if (+max == NaN) {
+      //TODO does not seem to enter this block
+      console.log(max + " is not a number");
+      alert("Please provide a number greater than 1");
+      return false;
+
+    } else {
+
+      return +max;
+
+    }
+  }
+
+  if (checkVaild(max)) {
+    for (var i = 0; i <=max; i++) {
+      var result = check(i);
+
+      $('ul').append('<li>'+result+'</li>');
+
+    }
+  }
+
+}
+
+$(document).ready(function($) {
+  $('button#go-button').click(function() {
+    var max = $('input:text').val();
+    $('input:text').val('');
+    $('ul').empty()
+    fizzbuzz(max);
+  });
 
 });
